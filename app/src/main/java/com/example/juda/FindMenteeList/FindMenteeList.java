e76233bed4dd407d96a66232cbc9b0ee128aedbe
LeeWonHee <pigmong02020202@gmail.com>
2023-06-29 04:04:18 +0900

    실제 데이터를 받기 전까지 샘플 데이터를 출력하겠습니다. firebase에서 데이터를 받는 부분은 모두 작성했습니다 데이터를 실제로 받으면 약간의 수정 후 출력하면 됩니다.

--- app/src/main/java/com/example/juda/FindMenteeList/FindMenteeList.java
+++ app/src/main/java/com/example/juda/FindMenteeList/FindMenteeList.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -17,44 +18,59 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FindMenteeList extends AppCompatActivity {
 
-    private DatabaseReference mDatabaseReference;
+    final private DatabaseReference mDatabaseReference = FirebaseDatabase.getInstance().getReference();
+    private FindMenteeListAdapter mAdapter = null;
     private EditText search_ET;
     private Button search_BTN, filter_BTN;
     private FloatingActionButton newPost_FAB;
     private ListView findMentee_LV;
-    private HashMap<String, String> menteePostHashMap;
+    private List<String> menteePostKey = null;
+    private List<String> menteePostTitle = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_mentee_list);
 
-        init();
+        Init();
     }
 
     /**
      * get ID and Connect
      */
-    private void init() {
+    private void Init() {
         search_ET = findViewById(R.id.search_ET_FindMenteeList);
         search_BTN = findViewById(R.id.search_BTN_FindMenteeList);
         filter_BTN = findViewById(R.id.filter_BTN_FindMenteeList);
         newPost_FAB = findViewById(R.id.newPost_FAB_FindMenteeList);
         findMentee_LV = findViewById(R.id.findMentee_LV_FindMenteeList);
 
-        mDatabaseReference = setDatabase();
+//        getMenteePostList();
+
+//        show sample data
+        setSampleAdapter();
     }
 
     /**
-     * This method returns FirebaseDatabase.getInstance().getReference()
-     * @return DatabaseReference
+     * This method show sample data on ListView
      */
-    private DatabaseReference setDatabase() {
-        return FirebaseDatabase.getInstance().getReference();
+    private void setSampleAdapter() {
+        List<String> sampleListData = new ArrayList<>();
+        sampleListData.add("post_1");
+        sampleListData.add("post_2");
+        sampleListData.add("post_3");
+        sampleListData.add("post_4");
+        sampleListData.add("post_5");
+        sampleListData.add("post_6");
+        sampleListData.add("post_7");
+        sampleListData.add("post_8");
+        ArrayAdapter<String> sampleAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, sampleListData);
+        findMentee_LV.setAdapter(sampleAdapter);
     }
 
     /**
@@ -67,12 +83,18 @@ public class FindMenteeList extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    menteePostHashMap.put(snapshot.getKey(), (String)snapshot.getValue());
+                    menteePostKey.add(snapshot.getKey());
+                    menteePostTitle.add((String)snapshot.getValue());
                 }
 
-                /*
-                ValueEventListener는 비동기 처리 되므로, 이 부분에 처리할 코드를 작성해야합니다!
-                 */
+//                ValueEventListener는 비동기 처리 되므로, 이 부분에 처리할 코드를 작성해야합니다!
+
+
+                mAdapter = new FindMenteeListAdapter(getApplicationContext(), menteePostTitle);
+                findMentee_LV.setAdapter(mAdapter); //FindMenteeList.java에서 코딩 필요합니다 - 그 전까지는 샘플 데이터를 출력하겠습니다!
+
+
+//                ValueEventListener는 비동기 처리 되므로, 이 부분에 처리할 코드를 작성해야합니다!
             }
 
             @Override
--- app/src/main/java/com/example/juda/FindMenteeList/FindMenteeListAdapter.java
+++ app/src/main/java/com/example/juda/FindMenteeList/FindMenteeListAdapter.java
@@ -4,25 +4,35 @@ This adapter class is for findMentee_LV in FindMenteeList.java
 
 package com.example.juda.FindMenteeList;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
+import java.util.List;
+
 public class FindMenteeListAdapter extends BaseAdapter {
+    private Context mContext = null;
+    List<String> mMenteePostTitle = null;
+
+    public FindMenteeListAdapter(Context mContext, List<String> menteePostTitle) {
+        this.mContext = mContext;
+        this.mMenteePostTitle = menteePostTitle;
+    }
 
     @Override
     public int getCount() {
-        return 0;
+        return mMenteePostTitle.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return mMenteePostTitle.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
