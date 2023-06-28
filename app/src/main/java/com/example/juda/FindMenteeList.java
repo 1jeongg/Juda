8a809e065542092d691958807322615a3dae5f36
LeeWonHee <pigmong02020202@gmail.com>
2023-06-29 03:00:41 +0900

    Add dependencies in build.gradle(app) Add setDatabase(), getMenteePostList() in FindMenteeList.java - Please read description

--- app/src/main/java/com/example/juda/FindMenteeList.java
+++ app/src/main/java/com/example/juda/FindMenteeList.java
@@ -1,26 +1,36 @@
 package com.example.juda;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class FindMenteeList extends AppCompatActivity {
 
+    private DatabaseReference mDatabaseReference;
     private EditText search_ET;
     private Button search_BTN, filter_BTN;
     private FloatingActionButton newPost_FAB;
     private ListView findMentee_LV;
+    private HashMap<String, String> menteePostHashMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_mentee_list);
-
-        init();
 
         init();
     }
@@ -34,5 +44,36 @@ public class FindMenteeList extends AppCompatActivity {
         filter_BTN = findViewById(R.id.filter_BTN_FindMenteeList);
         newPost_FAB = findViewById(R.id.newPost_FAB_FindMenteeList);
         findMentee_LV = findViewById(R.id.findMentee_LV_FindMenteeList);
+
+        mDatabaseReference = setDatabase();
+    }
+
+    /**
+     * This method returns FirebaseDatabase.getInstance().getReference()
+     * @return DatabaseReference
+     */
+    private DatabaseReference setDatabase() {
+        return FirebaseDatabase.getInstance().getReference();
+    }
+
+    /**
+     * This method gets data from firebase
+     * Get all data under 'MenteePost' and save all at menteePostHashMap
+     */
+    private void getMenteePostList() {
+        Query query = mDatabaseReference.child("MenteePost");
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    menteePostHashMap.put(snapshot.getKey(), (String)snapshot.getValue());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.d("PIGMONGKEY", "FindMenteeList.java_getMenteePostList() - onCancelled");
+            }
+        });
     }
 }
