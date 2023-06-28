99785d67286d513e044a6b963f825a5a12e39b06
LeeWonHee <pigmong02020202@gmail.com>
2023-06-29 00:38:15 +0900

    initial commit just make Acivity

---
+++ app/src/main/java/com/example/juda/FindMenteeList.java
@@ -0,0 +1,14 @@
+package com.example.juda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FindMenteeList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_mentee_list);
+    }
+}
