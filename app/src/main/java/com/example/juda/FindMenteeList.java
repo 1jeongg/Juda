e948dea2e4660ef4475d6e85bdc2df22769418fd
LeeWonHee <pigmong02020202@gmail.com>
2023-06-29 01:10:45 +0900

    Add ID (without 4 buttons) Add init() in FindMenteeList.java

--- app/src/main/java/com/example/juda/FindMenteeList.java
+++ app/src/main/java/com/example/juda/FindMenteeList.java
@@ -3,12 +3,35 @@ package com.example.juda;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class FindMenteeList extends AppCompatActivity {
+
+    EditText search_ET;
+    Button search_BTN, filter_BTN;
+    ListView findMentee_LV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_mentee_list);
+
+        init();
+
+        init();
     }
+
+    /**
+     * get ID and Connect
+     */
+    private void init() {
+        search_ET = findViewById(R.id.search_ET_FindMenteeList);
+        search_BTN = findViewById(R.id.search_BTN_FindMenteeList);
+        filter_BTN = findViewById(R.id.filter_BTN_FindMenteeList);
+        findMentee_LV = findViewById(R.id.findMentee_LV_FindMenteeList);
+    }
+
+
 }
