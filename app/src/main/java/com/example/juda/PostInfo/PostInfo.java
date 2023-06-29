545ec7e76022c27256500c751b5e5be45ed09781
LeeWonHee <pigmong02020202@gmail.com>
2023-06-29 12:23:54 +0900

    게시글 보여주기 창 PostInfo Activity 추가

---
+++ app/src/main/java/com/example/juda/PostInfo/PostInfo.java
@@ -0,0 +1,16 @@
+package com.example.juda.PostInfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.juda.R;
+
+public class PostInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_info);
+    }
+}
