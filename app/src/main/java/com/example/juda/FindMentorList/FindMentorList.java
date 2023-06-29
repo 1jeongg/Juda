cc70f384c0b6bfc09a2463250f1b329efb2b59d8
LeeWonHee <pigmong02020202@gmail.com>
2023-06-29 19:52:59 +0900

    멘토 찾는 글 리스트 화면 생성

---
+++ app/src/main/java/com/example/juda/FindMentorList/FindMentorList.java
@@ -0,0 +1,16 @@
+package com.example.juda.FindMentorList;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.juda.R;
+
+public class FindMentorList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_mentor_list);
+    }
+}
